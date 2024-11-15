8e3537d2d3e600fd004a5a24b1d3fedc0bb2b662
webhelper90 <webhelper90@gmail.com>
2024-11-15 17:37:25 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
-function click1(event){const onsNavigator1=document.getElementById('ons-navigator1');onsNavigator1.pushPage('page4.html');window.history.pushState({},'','page4.html')}
-document.addEventListener('init',(event)=>{let page=event.target;if(page.id==='ons-page5'){let onsbutton1_element=document.querySelector('#ons-button1');if(onsbutton1_element){onsbutton1_element.addEventListener('click',click1);page.addEventListener('destroy',function(event){onsbutton1_element.removeEventListener('click',click1)},{'once':!0})}}})
+function click1(event) {
+    /** @type{ons.OnsNavigatorElement} */
+    const onsNavigator1 = document.getElementById('ons-navigator1');
+    onsNavigator1.pushPage('page4.html');
+
+    // Androidでバックボタンを押したとき用にブラウザのヒストリーに追加する
+    // Androidでバックボタンを押したとき用にブラウザのヒストリーに追加する
+    window.history.pushState({}, '', 'page4.html');
+}
+
+document.addEventListener('init', (event) => {
+    let page = event.target;
+    if (page.id === 'ons-page5') {
+        let onsbutton1_element = document.querySelector('#ons-button1');
+        if(onsbutton1_element) {
+            onsbutton1_element.addEventListener('click', click1);
+            page.addEventListener('destroy', function(event) {
+                onsbutton1_element.removeEventListener('click', click1);
+            }, { 'once' : true });
+        }
+    }
+});

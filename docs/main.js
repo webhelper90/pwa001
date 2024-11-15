573cfca54e728045860665302fe3dac0a0049ae2
webhelper90 <webhelper90@gmail.com>
2024-11-15 18:23:07 +0900

    Add files via upload

--- docs/main.js
+++ docs/main.js
@@ -1,47 +1,20 @@
-function click1(event) {
-    // /** @type{ons.OnsNavigatorElement} */
-    // const onsNavigator1 = document.getElementById('ons-navigator1');
-    // onsNavigator1.pushPage('page4.html');
-
-    pushPage('page4.html')
-}
-
-function initApp(event) {
-var myNavigator = document.querySelector('#myNavigator'); // navigatorのIDを指定
-
-  ons.backbutton().on('backbutton', function(event) {
-    if (myNavigator.canPopPage()) { // 戻るページがあるか確認
-      myNavigator.popPage();
-    } else {
-      // ルートページにいる場合の処理
-      ons.notification.confirm({
-        message: 'アプリを終了しますか？',
-        buttonLabels: ['はい', 'いいえ'],
-        callback: function(idx) {
-          if (idx === 0) { // 'はい'が選択された場合
-            navigator.app.exitApp(); // アプリを終了 (Cordovaが必要)
-          }
-        }
-      });
-    }
-
-    // デフォルトの挙動をキャンセル
-    event.callParentHandler();
-  });
-}
-
 function popstate1(event) {
+    event.preventDefault();
     /** @type{ons.OnsNavigatorElement} */
     const onsNavigator1 = document.getElementById('ons-navigator1');
 
     if (onsNavigator1.pages.length > 1) {  // 戻るページがあるか確認
         onsNavigator1.popPage();
+        
+        //ブラウザのヒストリーから1つ除外する
+        history.go(-1); // ブラウザのヒストリーから1つ除外する
     } else {
         // ホームページの表示など、ルートページでの処理
         // 例: ホームページに遷移
-        window.location.href = '#page5'; // ホームページのURLを設定
-        onsNavigator1.resetPage('page5.html');  // home.htmlにリセット
+        //window.location.href = '#page5'; // ホームページのURLを設定
+        onsNavigator1.resetToPage('page5.html');  // home.htmlにリセット
     }
+
 }
 
 function pushPage(page) {
@@ -50,11 +23,28 @@ function pushPage(page) {
 
     const state = { page: page };
     const title = ''; // 必要に応じて設定
-    const url = `#${page}`; // ハッシュを使ってページを区別
-    history.pushState(state, title, url);
+    //const url = `#${page}`; // ハッシュを使ってページを区別
+    //history.pushState(state, title, url);
+    history.pushState(state, '');
     onsNavigator1.pushPage(page);
 }
 
+
+function click1(event) {
+    // /** @type{ons.OnsNavigatorElement} */
+    // const onsNavigator1 = document.getElementById('ons-navigator1');
+    // onsNavigator1.pushPage('page4.html');
+
+    pushPage('page4.html')
+}
+
+function load1(event) {
+    ons.ready(() => {
+        ons.setDefaultDeviceBackButtonListener((event) => {
+            popstate1(event);
+        });
+    });
+}
 
 window.addEventListener('popstate', popstate1);
 document.addEventListener('init', (event) => {
@@ -65,6 +55,18 @@ document.addEventListener('init', (event) => {
             onsbutton1_element.addEventListener('click', click1);
             page.addEventListener('destroy', function(event) {
                 onsbutton1_element.removeEventListener('click', click1);
+            }, { 'once' : true });
+        }
+    }
+});
+document.addEventListener('init', (event) => {
+    let page = event.target;
+    if (page.id === 'ons-page4') {
+        let onsbackbutton1_element = document.querySelector('#ons-back-button1');
+        if(onsbackbutton1_element) {
+            onsbackbutton1_element.addEventListener('click', popstate1);
+            page.addEventListener('destroy', function(event) {
+                onsbackbutton1_element.removeEventListener('click', popstate1);
             }, { 'once' : true });
         }
     }
